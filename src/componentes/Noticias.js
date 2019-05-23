6aa935bf6f2b8816234c10040695f64753fa7803
Cristian Clavijo <cristian982940@gmail.com>
2019-05-22 23:20:23 -0500

    conditional imagen yes empty

--- src/componentes/Noticias.js
+++ src/componentes/Noticias.js
@@ -1,18 +1,24 @@
 import React, {Component} from 'react';
 import Noticia from './Noticia';
 import PropTypes from 'prop-types';
+import {TransitionGroup, CSSTransition} from 'react-transition-group';  
 
 export default class Noticias extends Component{
   
   render(){
     return(
       <div className="row">
+        <TransitionGroup>
         {this.props.noticias.map(noticia => (
-          <Noticia
-            key = {noticia.url}
-            noticia = {noticia}
-          /> 
+          <CSSTransition
+            key = {noticia.url} 
+            classNames="fade"
+            timeout={500}
+          >
+            <Noticia noticia = {noticia} /> 
+          </CSSTransition>
         ))}
+        </TransitionGroup>
       </div>
     )
   }
